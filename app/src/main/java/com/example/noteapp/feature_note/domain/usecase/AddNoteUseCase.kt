6a858b375f5535f3d5406ca5fc7f49402d8b99ea
paulpriyam <ppaul2204@gmail.com>
2023-08-07 11:20:32 +0530

    1. Added AddNoteUseCase 2. Added noteFilter and orderType.

--- app/src/main/java/com/example/noteapp/feature_note/domain/usecase/AddNoteUseCase.kt
+++ app/src/main/java/com/example/noteapp/feature_note/domain/usecase/AddNoteUseCase.kt
@@ -1,10 +1,34 @@
 package com.example.noteapp.feature_note.domain.usecase
 
-import com.example.noteapp.feature_note.domain.model.InvalidNoteException
 import com.example.noteapp.feature_note.domain.model.Note
 import com.example.noteapp.feature_note.domain.repository.NoteRepository
+import com.example.noteapp.feature_note.domain.util.NoteFilter
+import com.example.noteapp.feature_note.domain.util.OrderType
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class AddNoteUseCase(private val repository: NoteRepository) {
 
-
+    operator fun invoke(
+        noteFilter: NoteFilter = NoteFilter.Date(OrderType.Descending)
+    ): Flow<List<Note>> {
+        return repository.getNotes().map { notes ->
+            when (noteFilter.orderType) {
+                OrderType.Ascending -> {
+                    when (noteFilter) {
+                        is NoteFilter.Date -> notes.sortedBy { it.timeStamp }
+                        is NoteFilter.Title -> notes.sortedBy { it.title }
+                        is NoteFilter.Color -> notes.sortedBy { it.color }
+                    }
+                }
+                OrderType.Descending -> {
+                    when (noteFilter) {
+                        is NoteFilter.Date -> notes.sortedByDescending { it.timeStamp }
+                        is NoteFilter.Title -> notes.sortedByDescending { it.title }
+                        is NoteFilter.Color -> notes.sortedByDescending { it.color }
+                    }
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/noteapp/feature_note/domain/util/NoteFilter.kt
+++ app/src/main/java/com/example/noteapp/feature_note/domain/util/NoteFilter.kt
@@ -1,4 +1,7 @@
 package com.example.noteapp.feature_note.domain.util
 
 sealed class NoteFilter(val orderType: OrderType) {
+    class Title(orderType: OrderType) : NoteFilter(orderType)
+    class Date(orderType: OrderType) : NoteFilter(orderType)
+    class Color(orderType: OrderType) : NoteFilter(orderType)
 }
