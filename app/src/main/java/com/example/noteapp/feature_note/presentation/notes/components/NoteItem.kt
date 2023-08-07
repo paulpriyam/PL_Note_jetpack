ec691d41f88c81517878c7612e8c507c9acadc47
paulpriyam <ppaul2204@gmail.com>
2023-08-07 17:16:19 +0530

    1. Added NoteItem component.

--- app/src/main/java/com/example/noteapp/feature_note/presentation/notes/components/NoteItem.kt
+++ app/src/main/java/com/example/noteapp/feature_note/presentation/notes/components/NoteItem.kt
@@ -28,8 +28,65 @@ import com.example.noteapp.ui.theme.LightGreen
 
 @Composable
 fun NoteItem(
+    note: Note,
+    modifier: Modifier = Modifier,
+    cardCornerRadius: Dp = 10.dp,
+    cutCornerRadius: Dp = 10.dp,
+    onDeleteClick: () -> Unit
 ) {
+    Box(modifier = modifier) {
+        Canvas(modifier = Modifier.matchParentSize()) {
+            val clipPath = Path().apply {
+                lineTo(size.width - cutCornerRadius.toPx(), 0f)
+                lineTo(size.width, cutCornerRadius.toPx())
+                lineTo(size.width, size.height)
+                lineTo(0f, size.height)
+                close()
+            }
+            clipPath(clipPath) {
+                drawRoundRect(
+                    color = Color(note.color),
+                    size = size,
+                    cornerRadius = CornerRadius(cardCornerRadius.toPx())
+                )
 
+                drawRoundRect(
+                    color = Color(ColorUtils.blendARGB(note.color, 0x000000, 0.2F)),
+                    size = Size(cutCornerRadius.toPx() + 100f, cutCornerRadius.toPx() + 100f),
+                    cornerRadius = CornerRadius(cardCornerRadius.toPx()),
+                    topLeft = Offset(size.width - cutCornerRadius.toPx(), -100f)
+                )
+            }
+
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+                .padding(end = 32.dp)
+        ) {
+            Text(
+                text = note.title,
+                style = MaterialTheme.typography.h6,
+                color = MaterialTheme.colors.onSurface,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = note.content,
+                style = MaterialTheme.typography.body1,
+                color = MaterialTheme.colors.onSurface,
+                maxLines = 10,
+                overflow = TextOverflow.Ellipsis
+            )
+            IconButton(onClick = onDeleteClick) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "delete note")
+            }
+        }
+    }
 }
 
 @Preview
