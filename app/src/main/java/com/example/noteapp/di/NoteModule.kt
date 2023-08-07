159bbe562cd905ee15d59d7203825f8737489541
paulpriyam <ppaul2204@gmail.com>
2023-08-07 11:41:48 +0530

    1. Added Dependency Injection

--- app/src/main/java/com/example/noteapp/di/NoteModule.kt
+++ app/src/main/java/com/example/noteapp/di/NoteModule.kt
@@ -2,13 +2,11 @@ package com.example.noteapp.di
 
 import android.app.Application
 import androidx.room.Room
+import com.example.noteapp.feature_note.data.data_source.NotesDao
 import com.example.noteapp.feature_note.data.data_source.NotesDatabase
 import com.example.noteapp.feature_note.data.repository.NoteRepositoryImpl
 import com.example.noteapp.feature_note.domain.repository.NoteRepository
-import com.example.noteapp.feature_note.domain.usecase.AddNoteUseCase
-import com.example.noteapp.feature_note.domain.usecase.DeleteNoteUseCase
-import com.example.noteapp.feature_note.domain.usecase.GetNoteUseCase
-import com.example.noteapp.feature_note.domain.usecase.NoteUsecase
+import com.example.noteapp.feature_note.domain.usecase.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -21,14 +19,29 @@ object NoteModule {
 
     @Provides
     @Singleton
-    fun providesNoteDatabase(app: Application): NotesDatabase {
+    fun provideNoteDatabase(app: Application): NotesDatabase {
         return Room.databaseBuilder(app, NotesDatabase::class.java, "Note_db").build()
     }
 
     @Provides
     @Singleton
-    fun providesNoteRepository(db: NotesDatabase): NoteRepository {
-        return NoteRepositoryImpl(db.notesDao)
+    fun provideNotesDao(db: NotesDatabase): NotesDao = db.notesDao
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(notesDao: NotesDao): NoteRepository {
+        return NoteRepositoryImpl(notesDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCase(noteRepository: NoteRepository): NoteUseCase {
+        return NoteUseCase(
+            getNoteUseCase = GetNoteUseCase(noteRepository),
+            addNoteUseCase = AddNoteUseCase(noteRepository),
+            getNoteByIdUseCase = GetNoteByIdUseCase(noteRepository),
+            deleteNoteUseCase = DeleteNoteUseCase(noteRepository)
+        )
     }
 
 }
--- app/src/main/java/com/example/noteapp/feature_note/domain/usecase/NoteUsecase.kt
+++ app/src/main/java/com/example/noteapp/feature_note/domain/usecase/NoteUsecase.kt
@@ -1,3 +1,8 @@
 package com.example.noteapp.feature_note.domain.usecase
 
-
+data class NoteUseCase(
+    val getNoteUseCase: GetNoteUseCase,
+    val addNoteUseCase: AddNoteUseCase,
+    val deleteNoteUseCase: DeleteNoteUseCase,
+    val getNoteByIdUseCase: GetNoteByIdUseCase
+)
