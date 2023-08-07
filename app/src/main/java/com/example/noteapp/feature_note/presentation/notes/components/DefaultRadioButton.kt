b79315c1fe880cdffe5a4226b2d2c29fa806ed23
paulpriyam <ppaul2204@gmail.com>
2023-08-07 13:38:50 +0530

    1. Added DefaultRadioButton component.

--- app/src/main/java/com/example/noteapp/feature_note/presentation/notes/components/DefaultRadioButton.kt
+++ app/src/main/java/com/example/noteapp/feature_note/presentation/notes/components/DefaultRadioButton.kt
@@ -11,5 +11,24 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun DefaultRadioButton(
+    title: String,
+    isSelected: Boolean,
+    onCheck: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(
+            selected = isSelected,
+            onClick = onCheck,
+            colors = RadioButtonDefaults.colors(
+                selectedColor = MaterialTheme.colors.primary,
+                unselectedColor = MaterialTheme.colors.onBackground
+            )
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = title, style = MaterialTheme.typography.body1)
+    }
 }
