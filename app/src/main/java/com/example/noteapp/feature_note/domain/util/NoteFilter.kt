b57875a1252245ec80dc97f6eda549e55151fe41
paulpriyam <ppaul2204@gmail.com>
2023-08-07 13:57:30 +0530

    1. Added NoteFilterSection component.

--- app/src/main/java/com/example/noteapp/feature_note/domain/util/NoteFilter.kt
+++ app/src/main/java/com/example/noteapp/feature_note/domain/util/NoteFilter.kt
@@ -4,4 +4,12 @@ sealed class NoteFilter(val orderType: OrderType) {
     class Title(orderType: OrderType) : NoteFilter(orderType)
     class Date(orderType: OrderType) : NoteFilter(orderType)
     class Color(orderType: OrderType) : NoteFilter(orderType)
+
+    fun copy(orderType: OrderType): NoteFilter {
+        return when (this) {
+            is Title -> Title(orderType)
+            is Date -> Date(orderType)
+            is Color -> Color(orderType)
+        }
+    }
 }
--- app/src/main/java/com/example/noteapp/feature_note/presentation/notes/components/FilterSection.kt
+++ app/src/main/java/com/example/noteapp/feature_note/presentation/notes/components/FilterSection.kt
@@ -1,7 +1,9 @@
 package com.example.noteapp.feature_note.presentation.notes.components
 
+import android.widget.Space
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.noteapp.feature_note.domain.util.NoteFilter
@@ -9,7 +11,43 @@ import com.example.noteapp.feature_note.domain.util.OrderType
 
 @Composable
 fun NoteFilterSection(
-
+    modifier: Modifier = Modifier,
+    noteFilter: NoteFilter = NoteFilter.Date(OrderType.Descending),
+    onChangeFilter: (NoteFilter) -> Unit
 ) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            DefaultRadioButton(
+                title = "Title",
+                isSelected = noteFilter is NoteFilter.Title,
+                onCheck = { onChangeFilter(NoteFilter.Title(noteFilter.orderType)) })
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                title = "Date",
+                isSelected = noteFilter is NoteFilter.Date,
+                onCheck = { onChangeFilter(NoteFilter.Date(noteFilter.orderType)) })
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                title = "Color",
+                isSelected = noteFilter is NoteFilter.Color,
+                onCheck = { onChangeFilter(NoteFilter.Color(noteFilter.orderType)) })
 
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(modifier = Modifier.fillMaxWidth()) {
+            DefaultRadioButton(
+                title = "Ascending",
+                isSelected = noteFilter.orderType is OrderType.Ascending,
+                onCheck = { noteFilter.copy(OrderType.Ascending) })
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(
+                title = "Descending",
+                isSelected = noteFilter.orderType is OrderType.Descending,
+                onCheck = { noteFilter.copy(OrderType.Descending) })
+        }
+    }
 }
