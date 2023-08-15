4964ede993e4eccfcf4b5945b439051ae70de993
paulpriyam <ppaul2204@gmail.com>
2023-08-15 11:19:26 +0530

    1. Added GetNoteByIdUseCase.

--- app/src/main/java/com/example/noteapp/feature_note/domain/usecase/GetNoteByIdUseCase.kt
+++ app/src/main/java/com/example/noteapp/feature_note/domain/usecase/GetNoteByIdUseCase.kt
@@ -5,4 +5,7 @@ import com.example.noteapp.feature_note.domain.repository.NoteRepository
 
 class GetNoteByIdUseCase(private val repository: NoteRepository) {
 
+    suspend operator fun invoke(id: Int): Note? {
+        return repository.getNoteById(id)
+    }
 }
